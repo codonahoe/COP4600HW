2d78f55dec3e3e7eb3ef8b893442142db6b2ae2b
Talia Martin <67079575+deerly-tali@users.noreply.github.com>
2024-02-15 21:21:23 -0500

    rename vars for easier to read

--- COP4600HW1.py
+++ COP4600HW1.py
@@ -4,43 +4,43 @@
 #TODO: test & debug
 
 class Process: #structure to represent a processes
-    def __init__(self, name, arrival_time, execution_time):
-        self.arrival_time = arrival_time
-        self.execution_time = execution_time
+    def __init__(self, name, arrival, burst):
+        self.arrival = arrival
+        self.burst = burst
         self.name = name
         self.status = "Ready"
-        self.start_time = None
-        self.finish_time = None
-        self.response_time = None
-        self.wait_time = None
+        self.start = None
+        self.finish = None
+        self.response = None
+        self.wait = None
 
 def FIFO_Scheduler(processes): #first in first out
     cur_time = 0
     #for each process in our queue
     for process in processes:
-        if process.arrival_time > cur_time: #then the current time is updated since it is now first
-            cur_time = process.arrival_time
-        process.start_time = current_time
-        process.response_time = current_time - process.arrival_time
-        process.finish_time = current_time + process.execution_time
-        process.wait_time = process.start_time - process.arrival_time
-        current_time = process.finish_time
+        if process.arrival > cur_time: #then the current time is updated since it is now first
+            cur_time = process.arrival
+        process.start = current_time
+        process.response = current_time - process.arrival
+        process.finish = current_time + process.burst
+        process.wait = process.start - process.arrival
+        current_time = process.finish
 
 def SJF_Scheduler(processes): #shortest job first
-    processes.sort(key=lambda x: (x.arrival_time, x.execution_time))
+    processes.sort(key=lambda x: (x.arrival_time, x.burst))
     cur_time = 0
     remaining_processes = processes.copy()
     while remaining_processes:
-        next_process = min(remaining_processes, key=lambda x: x.execution_time)
-        if next_process.arrival_time > cur_time:
-            cur_time = next_process.arrival_time
-        next_process.start_time = cur_time
-        next_process.response_time = cur_time - next_process.arrival_time
+        next_process = min(remaining_processes, key=lambda x: x.burst)
+        if next_process.arrival > cur_time:
+            cur_time = next_process.arrival
+        next_process.start = cur_time
+        next_process.response = cur_time - next_process.arrival
         cur_time += 1
-        next_process.execution_time -= 1
-        if next_process.execution_time == 0:
-            next_process.finish_time = cur_time
-            next_process.wait_time = next_process.start_time - next_process.arrival_time
+        next_process.burst -= 1
+        if next_process.burst == 0:
+            next_process.finish = cur_time
+            next_process.wait = next_process.start - next_process.arrival
             remaining_processes.remove(next_process)
 
 def RR_Scheduler(processes, q_value): #round robin
@@ -48,36 +48,36 @@ def RR_Scheduler(processes, q_value): #round robin
     remaining_processes = processes.copy()
     while remaining_processes:
         for process in remaining_processes:
-            if process.arrival_time <= cur_time:
-                process.start_time = cur_time
-                process.response_time = cur_time - process.arrival_time
-                if process.execution_time <= q_value:
-                    cur_time += process.execution_time
-                    process.finish_time = cur_time
-                    process.wait_time = process.start_time - process.arrival_time
+            if process.arrival <= cur_time:
+                process.start = cur_time
+                process.response = cur_time - process.arrival
+                if process.burst <= q_value:
+                    cur_time += process.burst
+                    process.finish = cur_time
+                    process.wait = process.start - process.arrival
                     remaining_processes.remove(process)
                 else:
                     cur_time += q_value
-                    process.execution_time -= q_value
+                    process.burst -= q_value
             else:
                 cur_time += 1
 
 
 def calculate_performance(processes): #find out how turnaround, wait time,and response time performs
-    turnaround_time = 0
-    response_time = 0
-    wait_time = 0
+    turnaround = 0
+    response = 0
+    wait = 0
     #find total times of each process in order to get an average
     for process in processes:
-        turnaround_time += process.finish_time - process.arrival_time
-        wait_time += process.wait_time
-        response_time += process.response_time
+        turnaround += process.finish - process.arrival
+        wait += process.wait
+        response += process.response
     #calculate the averages
     num_processes = len(processes) 
-    avg_turnaround_time = turnaround_time / num_processes
-    avg_wait_time = wait_time / num_processes
-    avg_response_time = response_time / num_processes
-    return avg_turnaround_time, avg_wait_time, avg_response_time
+    avg_turnaround = turnaround / num_processes
+    avg_wait = wait / num_processes
+    avg_response = response / num_processes
+    return avg_turnaround, avg_wait, avg_response
 
 def read_processes_from_file(filename):
     processes = []
@@ -98,8 +98,8 @@ def read_processes_from_file(filename):
             elif parts[0] == 'quantum':
                 quantum = int(parts[1])
             elif parts[0] == 'process':
-                name, arrival_time, execution_time = parts[1], int(parts[2]), int(parts[3])
-                processes.append(Process(name, arrival_time, execution_time))
+                name, arrival_time, burst = parts[1], int(parts[2]), int(parts[3])
+                processes.append(Process(name, arrival_time, burst))
             elif parts[0] == 'end':
                 break
     return processcount, runfor, use, quantum, processes
