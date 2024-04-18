b9407a7f06bd338bb0ac758cd24200442373df28
codonahoe <codonahoe@gmail.com>
2024-04-18 11:08:09 -0400

    removed if linux from common_threads

--- common_threads.h
+++ common_threads.h
@@ -6,9 +6,9 @@
 #include <assert.h>
 #include <sched.h>
 
-#ifdef __linux__
+
 #include <semaphore.h>
-#endif
+
 
 #define Pthread_create(thread, attr, start_routine, arg) assert(pthread_create(thread, attr, start_routine, arg) == 0);
 #define Pthread_join(thread, value_ptr)                  assert(pthread_join(thread, value_ptr) == 0);
@@ -25,10 +25,9 @@
 #define Cond_signal(cond)                                assert(pthread_cond_signal(cond) == 0);
 #define Cond_wait(cond, mutex)                           assert(pthread_cond_wait(cond, mutex) == 0);
 
-#ifdef __linux__
+
 #define Sem_init(sem, value)                             assert(sem_init(sem, 0, value) == 0);
 #define Sem_wait(sem)                                    assert(sem_wait(sem) == 0);
 #define Sem_post(sem)                                    assert(sem_post(sem) == 0);
-#endif // __linux__
 
 #endif // __common_threads_h__
